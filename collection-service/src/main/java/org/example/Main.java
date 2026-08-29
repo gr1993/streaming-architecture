28557d98e1e04f9d9ab6a79987b3ef6ccda1402b
park kang lim <gr1993@naver.com>
2026-08-29 14:17:13 +0900

    feat: 수집 서비스 웹소켓 처리 핸들러 구현

--- collection-service/src/main/java/org/example/Main.java
+++ collection-service/src/main/java/org/example/Main.java
@@ -1,7 +1,85 @@
 package org.example;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.DefaultHttpHeaders;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketClientCompressionHandler;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+/**
+ * Netty 웹소켓 클라이언트 실행 코드 작성
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    private static boolean continueRunning = true;
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        final String URL =  0 < args.length? args[0] : "ws://localhost:8080/rsvp";
+
+        URI uri = new URI(URL);
+
+        try {
+            // 우리가 구현한 수집 핸들러 생성
+            final MeetupWebSocketClientHandler handler =
+                    new MeetupWebSocketClientHandler(
+                            WebSocketClientHandshakerFactory.newHandshaker(
+                                    uri, WebSocketVersion.V13, null, false, new DefaultHttpHeaders()));
+
+            // 파이프라인 설정
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new HttpClientCodec());
+                            p.addLast(new HttpObjectAggregator(8192));
+                            p.addLast(WebSocketClientCompressionHandler.INSTANCE);
+                            p.addLast(handler);
+                        }
+                    });
+
+            // 백그라운드에서 수집 단계 애플리케이션 실행하고
+            // Meetup API 서버와 연동되어 메시지가 전달되기까지 기다린다.
+            Channel ch = b.connect(uri.getHost(), uri.getPort()).sync().channel();
+            // 애플리케이션이 종료될 때까지 block 상태로 대기
+            handler.handshakeFuture().sync();
+
+            // 윕소켓의 핑 설정과 종료 메시지 처리
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            do {
+                String msg = console.readLine();
+                if (msg == null) {
+                    continueRunning = false;
+                } else if ("bye".equals(msg.toLowerCase())) {
+                    ch.writeAndFlush(new CloseWebSocketFrame());
+                    ch.closeFuture().sync();
+                    continueRunning = false;
+                } else if ("ping".equals(msg.toLowerCase())) {
+                    WebSocketFrame frame = new PingWebSocketFrame(Unpooled.wrappedBuffer(new byte[] { 8, 1, 8, 1 }));
+                    ch.writeAndFlush(frame);
+                } else {
+                    WebSocketFrame frame = new TextWebSocketFrame(msg);
+                    ch.writeAndFlush(frame);
+                }
+            }while (continueRunning);
+        } finally {
+            group.shutdownGracefully();
+        }
     }
 }
---
+++ collection-service/src/main/java/org/example/MeetupWebSocketClientHandler.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import io.netty.channel.*;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshaker;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.util.CharsetUtil;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+public class MeetupWebSocketClientHandler extends SimpleChannelInboundHandler {
+
+    private final WebSocketClientHandshaker handshaker;
+    private ChannelPromise handshakeFuture;
+
+    MeetupWebSocketClientHandler(WebSocketClientHandshaker handshaker) {
+        this.handshaker = handshaker;
+    }
+
+    ChannelFuture handshakeFuture() {
+        return handshakeFuture;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        Channel channel = ctx.channel();
+
+        // 웹소켓 핸드쉐이킹이 완료되었는지 검증
+        if (!handshaker.isHandshakeComplete()) {
+            //API 서버와 연동 시작
+            handshaker.finishHandshake(channel, (FullHttpResponse) msg);
+            handshakeFuture.setSuccess();
+            System.out.println("WebSocket Client connected and ready to consume RSVPs!");
+            return;
+        }
+
+        // 핸드쉐이킹 전에는 FullHttpResponse가 정상적인 HTTP Upgrade 응답
+        // 반면 핸드쉐이킹이 완료된 이후에는 WebSocket 통신이 이루어져야 하므로 FullHttpResponse가 들어오면 예상하지 못한 HTTP 응답으로 간주해서 예외를 발생
+        if (msg instanceof FullHttpResponse) {
+            FullHttpResponse response = (FullHttpResponse) msg;
+            throw new IllegalStateException(
+                    "Unexpected FullHttpResponse (getStatus=" + response.status() +
+                            ", content=" + response.content().toString(CharsetUtil.UTF_8) + ')');
+        }
+
+        WebSocketFrame frame = (WebSocketFrame) msg;
+        if (frame instanceof TextWebSocketFrame) {
+            TextWebSocketFrame textFrame = (TextWebSocketFrame) frame;
+
+            //메시지 키에 임의 값 사용
+            final String messageKey = UUID.randomUUID().toString();
+            final byte[] messagePayload = new byte[textFrame.content().readableBytes()];
+            textFrame.content().readBytes(messagePayload);
+
+            //HybridMessageLogger.addEvent(messageKey,messagePayload);
+            //rsvpProducer.sendMessage(messageKey,messagePayload);
+
+            System.out.println("read : " + new String(messagePayload, StandardCharsets.UTF_8));
+
+        } else if (frame instanceof CloseWebSocketFrame) {
+            channel.close();
+            //rsvpProducer.close();
+        }
+    }
+}
